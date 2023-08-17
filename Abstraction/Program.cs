2ff28f96b294bd9352cbf7c4f5b41c7df88eab42
girishjondhale <girishjondhale1997@gmail.com>
2023-08-16 17:32:05 -0700

    Add project files.

---
+++ Abstraction/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstraction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rectangle = new Rectangle(20,25);
+            rectangle.Calculatearea();
+            Console.WriteLine(rectangle);
+        }
+    }
+}
---
+++ Abstraction/Rectangle.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstraction
+{
+    public abstract class Abstract
+    {
+        public abstract void Calculatearea();
+    }
+
+    public class Rectangle : Abstract
+    {
+        public int length, breadth;
+        public int area;
+        public Rectangle(int length, int breadth)
+        { 
+            this.length = length;
+            this.breadth = breadth;
+        }
+        public override void Calculatearea()
+        { 
+            area=length*breadth;
+        }
+
+        public override string ToString()
+        {
+            return$"area of rectangle={area }"; 
+        }
+
+
+    }
+}
